177fec8d2e2d0e04d9b8910fbf009d17feefd6c9
Chris Wilding <hello@chriswilding.co.uk>
2022-02-27 16:53:48 +0000

    Add gorilla mux (#4)

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!")) //nolint:errcheck
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", http.HandlerFunc(hello)).Methods(http.MethodGet)
+	err := http.ListenAndServe(":8080", r)
+	log.Fatal(err)
 }
