c820a6258b0abb8bfd24431c49160470d441e2f8
Chris Wilding <hello@chriswilding.co.uk>
2022-02-27 16:52:08 +0000

    Resolve lint errors

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
+	w.Write([]byte("Hello, World!")) // //nolint:errcheck
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", http.HandlerFunc(hello)).Methods(http.MethodGet)
-	http.ListenAndServe(":8080", r)
+	err := http.ListenAndServe(":8080", r)
+	log.Fatal(err)
 }
